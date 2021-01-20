14a43e505f5573bdadf291e9b6ee6efdbb143a03
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2021-01-20 13:18:59 -0800

    finished my level added doors

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public GameObject door;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        door.SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+}
